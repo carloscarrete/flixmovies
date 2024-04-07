e1a5228ee28ee42ef15e1bde322bd2569f256255
Carlos Carrete <carloscarrete,sc@gmail.com>
2024-04-06 23:28:07 -0600

    Added Drawer Navigator

--- babel.config.js
+++ babel.config.js
@@ -2,12 +2,13 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ["nativewind/babel", [
+    plugins: ["nativewind/babel", 'react-native-reanimated/plugin',[
       'module:react-native-dotenv',
       {
         moduleName: 'react-native-dotenv',
         verbose: false,
       },
-    ],],
+    ],
+  ],
   };
 };
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -8,7 +8,7 @@ import { styles } from '../theme/'
 import { Bars3CenterLeftIcon, MagnifyingGlassIcon } from 'react-native-heroicons/outline'
 import TrendingMovies from '../components/TrendingMovies';
 import MovieList from '../components/MovieList'
-import { ParamListBase, useNavigation } from '@react-navigation/native'
+import { DrawerActions, ParamListBase, useNavigation } from '@react-navigation/native'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 import Loading from '../components/LoadingC';
 import LoadingC from '../components/LoadingC'
@@ -31,39 +31,39 @@ const HomeScreen = () => {
         getTrendingMovies();
         getUpcomingMovies();
         getRatedMovies();
-      /*   getNowPlayingMovies();
-        getPopularMovies(); */
+        /*   getNowPlayingMovies();
+          getPopularMovies(); */
     }, [])
-    
-  /* Get movies */
+
+    /* Get movies */
     const getTrendingMovies = async () => {
         const data = await fetchTrendingMovies();
-        if(data&& data.results.length > 0) setTrendingMovies(data.results) 
+        if (data && data.results.length > 0) setTrendingMovies(data.results)
         setLoading(false)
         return data
     }
 
     const getUpcomingMovies = async () => {
         const data = await fetchUpcoming();
-        if(data&& data.results.length > 0) setUpcominngMovies(data.results) 
+        if (data && data.results.length > 0) setUpcominngMovies(data.results)
         return data
     }
 
     const getRatedMovies = async () => {
         const data = await fetchTopRated();
-        if(data&& data.results.length > 0) setTopRatedMovies(data.results)
+        if (data && data.results.length > 0) setTopRatedMovies(data.results)
         return data
     }
-    
+
     const getNowPlayingMovies = async () => {
         const data = await fetchNowPlaying();
-         if(data&& data.results.length > 0) setTrendingMovies(data.results) 
+        if (data && data.results.length > 0) setTrendingMovies(data.results)
         return data
     }
 
     const getPopularMovies = async () => {
         const data = await fetchPopularMovies();
-        if(data&& data.results.length > 0) setTrendingMovies(data.results) 
+        if (data && data.results.length > 0) setTrendingMovies(data.results)
         return data
     }
 
@@ -72,7 +72,9 @@ const HomeScreen = () => {
             <SafeAreaView className={ios ? '-mb-2' : '-mb-3'}>
                 <StatusBar style='dark' />
                 <View className='flex-row items-center justify-between px-4 py-6 md:py-8'>
-                    <Bars3CenterLeftIcon size={24} color='white' strokeWidth={2} />
+                    <TouchableOpacity onPress={() => navigation.dispatch(DrawerActions.openDrawer())}>
+                        <Bars3CenterLeftIcon size={24} color='white' strokeWidth={2} />
+                    </TouchableOpacity>
                     <Text className='text-white font-bold text-2xl' style={styles.secondaryText}>
                         <Text style={styles.text}>Flix</Text>Finder
                     </Text>
@@ -90,19 +92,19 @@ const HomeScreen = () => {
                     <LoadingC />
                     :
                     <ScrollView
-                    showsVerticalScrollIndicator={false}
-                    contentContainerStyle={{ paddingBottom: 10 }}
-    
-                >
-    
-                    {/* Carrusel Trending Movies */}
-                    {  trendingMovies && <TrendingMovies data={trendingMovies} />   }
-                    {/* Upcoming Movies */}
-                   { upcomingMovies && <MovieList title="Upcoming Movies" data={upcomingMovies} />}
-                    {/* Top Rated Movies */}
-                   { topRatedMovies && <MovieList title="Top Rated Movies" data={topRatedMovies} />}
-    
-                </ScrollView>
+                        showsVerticalScrollIndicator={false}
+                        contentContainerStyle={{ paddingBottom: 10 }}
+
+                    >
+
+                        {/* Carrusel Trending Movies */}
+                        {trendingMovies && <TrendingMovies data={trendingMovies} />}
+                        {/* Upcoming Movies */}
+                        {upcomingMovies && <MovieList title="Upcoming Movies" data={upcomingMovies} />}
+                        {/* Top Rated Movies */}
+                        {topRatedMovies && <MovieList title="Top Rated Movies" data={topRatedMovies} />}
+
+                    </ScrollView>
             }
 
 
